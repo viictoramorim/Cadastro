34b3355baa7c2ba00d42b30e68e4b46effdda39d
viictoramorim <joaofarinon10@gmail.com>
2026-08-23 17:06:44 -0300

    Inicio do Projeto

---
+++ test/java/dev/amorim/Cadastros/CadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amorim.Cadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
