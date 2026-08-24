3f7ab3a0a324774aaec6249db02369de0086e24d
viictoramorim <joaofarinon10@gmail.com>
2026-08-23 21:18:18 -0300

    Inicio do Model e modificação do nome do Controller

---
+++ main/java/dev/amorim/Cadastros/PessoasModel.java
@@ -0,0 +1,38 @@
+package dev.amorim.Cadastros;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class PessoasModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private int years;
+
+
+    public PessoasModel(){
+    }
+
+    public PessoasModel(String nome, String email, int years){
+        this.name = name;
+        this.email = email;
+        this.years = years;
+    }
+
+    public Long getId(){return id;}
+    public void setId(Long Id){this.id = id;}
+
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email = email;}
+
+    public int getYears(){return years;}
+    public void setYears(int years){this.years = years;}
+}
